5c0f1f1edfe6b908c2e82371984d3318794fc109
michmoosch <michael.moore01@sjsu.edu>
2023-03-04 15:54:32 -0800

    Update

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        catamaran: ['"Work Sans"', "sans-serif"],
+      },
+      backgroundImage: {
+        wave: "url('./assets/wave.jpg')",
+      },
+      transform: [{ tuck: "translateZ(-1px)" }],
+    },
   },
   plugins: [],
 };
